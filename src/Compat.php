1f4c33ee917346a019862396d665cd0b5eff38dc
Tristan Lins <tristan.lins@bit3.de>
2014-03-26 22:01:19 +0100

    Add support for Contao 3.2.

--- src/Compat.php
+++ src/Compat.php
@@ -3,7 +3,12 @@
 /**
  * Class Compat
  */
-if (version_compare(VERSION, '3', '>=')) {
+if (version_compare(VERSION, '3.2', '>=')) {
+	class Compat extends \Compat\Contao3_2\Compat
+	{
+	}
+}
+elseif (version_compare(VERSION, '3', '>=')) {
 	class Compat extends \Compat\Contao3_0\Compat
 	{
 	}
---
+++ src/Compat/Contao3_2/Compat.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Compat\Contao3_2;
+
+use Controller;
+use Database;
+use FilesModel;
+use Model\Collection;
+use Compat\Contao3_0\Compat as Compat3_0;
+
+class Compat extends Compat3_0
+{
+	/**
+	 * @var Compat
+	 */
+	static protected $instance = null;
+
+	/**
+	 * @return Compat
+	 */
+	static protected function getInstance()
+	{
+		if (static::$instance === null) {
+			static::$instance = new Compat();
+		}
+		return static::$instance;
+	}
+
+	/**
+	 * @param $file
+	 *
+	 * @return bool|FilesModel
+	 * @throws \RuntimeException
+	 */
+	static public function getFileModel($file)
+	{
+		/**
+		 * Get file by DBAFS UUID
+		 */
+		if (\Validator::isUuid($file)) {
+			$file = FilesModel::findByUuid($file, array('uncached' => true));
+		}
+
+		/**
+		 * Search the old way
+		 */
+		else {
+			return parent::getFileModel($file);
+		}
+
+		/**
+		 * Get path from model
+		 */
+		if ($file instanceof FilesModel) {
+			return $file;
+		}
+
+		return false;
+	}
+}
